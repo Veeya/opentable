855c7c278a7305b343dc1ae12c31957b2bb5dd43
Joe Schwendt <joe@schwendt.com>
2018-05-22 09:59:57 -0400

    Fixed serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -7,33 +7,29 @@ class RestaurantSerializer
 
   def to_hash
     {
-      "id"                 => record.rid,
-      "name"               => record.name,
-      "address"            => record.address,
-      "city"               => record.city,
-      "state"              => record.state,
-      "area"               => record.metro_name,
-      "postal_code"        => record.postal_code,
-      "country"            => record.country,
-      "phone"              => record.phone,
-      "lat"                => record.latitude,
-      "lng"                => record.longitude,
-      "price"              => record.price,
-      "reserve_url"        => reserve_url,
-      "mobile_reserve_url" => mobile_reserve_url,
-      "image_url"          => image_url
+      "id"                     => record.rid,
+      "name"                   => record.name,
+      "address"                => record.address,
+      "address2"               => record.address2,
+      "city"                   => record.city,
+      "state"                  => record.state,
+      "postal_code"            => record.postal_code,
+      "area"                   => record.metro_name,
+      "country"                => record.country,
+      "phone"                  => record.phone,
+      "lat"                    => record.latitude,
+      "lng"                    => record.longitude,
+      "price_quartile"         => record.price_quartile,
+      "reservation_url"        => reservation_url,
+      "profile_url"            => profile_url,
+      "is_restaurant_in_group" => is_restaurant_in_group,
+      "aggregate_score"        => aggregate_score,
+      "review_count"           => review_count,
+      "image_url"              => image_url
     }
   end
 
   private
-
-  def reserve_url
-    "http://www.opentable.com/single.aspx?rid=#{record.rid}"
-  end
-
-  def mobile_reserve_url
-    "http://mobile.opentable.com/opentable/?restId=#{record.rid}"
-  end
 
   def image_url
     "https://www.opentable.com/img/restimages/#{record.rid}.jpg"
